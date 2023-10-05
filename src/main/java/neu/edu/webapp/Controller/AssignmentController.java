9ef75cc0c1413af7ac72157e73e7482448038b2f
sangram vuppala <vuppala.s@northeastern.edu>
2023-10-04 20:35:54 -0400

    Assignment DAO and update in GitHub Actions File

--- src/main/java/neu/edu/webapp/Controller/AssignmentController.java
+++ src/main/java/neu/edu/webapp/Controller/AssignmentController.java
@@ -1,5 +1,21 @@
 package neu.edu.webapp.Controller;
 
+import neu.edu.webapp.Model.Assignment;
+import neu.edu.webapp.Service.AssignmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class AssignmentController {
+
+    @Autowired
+    AssignmentService assignmentService;
+
+//    @GetMapping("/v1/assignments")
+//    public ResponseEntity<List<Assignment>> getAllAssignmentsByID()
 
 }
--- src/main/java/neu/edu/webapp/Service/AssignmentService.java
+++ src/main/java/neu/edu/webapp/Service/AssignmentService.java
@@ -1,9 +1,13 @@
 package neu.edu.webapp.Service;
 
+import neu.edu.webapp.DAO.AssignmentDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AssignmentService {
+    @Autowired
+    AssignmentDAO assignmentDAO;
 
-
+//    public boolean
 }
